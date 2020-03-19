4c4558d088dc2d263dac2fa9bf43508bb51593fa
rpdroky <rpdroky@hotmail.com>
2020-03-19 08:26:52 +0200

    Bringup APK squisher [1/2] Other commit is in make/core
    Please note that you may need to give permission to make it run
    
    chmod 777 squisher
    chmod 777 crusher
    chmod 777 colors
    
    Credits to Cl3kener and FusionJacks for the original commits
    Credits to frap129 for compatibility fixes
    
    Change-Id: Idca5ccbf2bb497ba807b155b561b5f164008363a

--- config/common.mk
+++ config/common.mk
@@ -375,3 +375,6 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/lineage/tools/squisher
